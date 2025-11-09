72cfe02f0466b9765d1e97ac1869ce04a4411401
harshlohia92 <lohiaharsh00@gmail.com>
2025-11-09 12:16:30 +0530

    Update server.py

--- server.py
+++ server.py
@@ -43,3 +43,10 @@ def recommend(query: str = Query(..., description="Job role or skill to recommen
 
     except Exception as e:
         return JSONResponse(content={"error": str(e)}, status_code=500)
+
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=port)
